c8604fd5c5592ba9914d4efefcc4b3a05072bb6c
panchukda <panch-dima@yandex.ru>
2020-10-29 22:48:08 +0300

    init

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+import time
+from rq import Queue
+from redis import Redis
+from flask import Flask, render_template
+from tasks import load_patients
+import json
+functions = {"load_patients": load_patients}
+app = Flask(__name__)
+queue = Queue(connection=Redis(), default_timeout=3600)
+
+@app.route('/')
+def hello_world():
+    List_commands = ["load_patients"]
+    return render_template('index.html', my_string="Список доступных комманд", List_commands=List_commands)
+
+@app.route('/task/<func>')
+def create_task(func):
+    job = queue.enqueue(functions["load_patients"], result_ttl=86400)
+    return job.id
+
+@app.route('/result/<id>')
+def result_task(id):
+    try:
+        job = queue.fetch_job(id)
+        print(job.get_status())
+        if job.is_finished:
+            return json.dumps(job.result, ensure_ascii=False)
+        else:
+            return 'Not ready', 202
+    except:
+        return "Not found", 404
+@app.route('/queue')
+def get_list_queue():
+    queued_job_ids = queue.job_ids
+    print(queued_job_ids)
+    return "queued"
+@app.route('/queue/clear')
+def clear_queue():
+    queue.empty()
+    return "Clear"
+
+if __name__ == '__main__':
+    app.run()
---
+++ tasks.py
@@ -0,0 +1,28 @@
+import requests
+import cx_Oracle
+import json
+import os
+os.environ['LD_LIBRARY_PATH'] = "/opt/oracle/product/18c/dbhomeXE/lib"
+os.environ['ORACLE_HOME'] = "/opt/oracle/product/18c/dbhomeXE"
+username = os.environ['ORACLE_USERNAME']
+password = os.environ['ORACLE_PASSWORD']
+database = os.environ['ORACLE_DATABASE']
+def count_words_at_url():
+
+    return "adasdadsasd"
+
+
+
+def load_patients():
+    data = {"data":[]}
+    with cx_Oracle.connect(username, password, database) as connection:
+        cursor = connection.cursor()
+        for result in cursor.execute('select p.keyid, p.firstname, p.lastname, p.secondname from patient p'):
+            data["data"].append(
+                {
+                 "keyid": result[0],
+                 "firstname": result[1],
+                 "lastname": result[2],
+                 "secondname": result[3]
+                 })
+        return json.dumps(data)
