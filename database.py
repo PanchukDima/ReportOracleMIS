82925191bf282a95d3d9b8025a2fb77e296766ae
panchukda <panch-dima@yandex.ru>
2020-10-30 12:59:55 +0300

    commit

--- database.py
+++ database.py
@@ -9,16 +9,18 @@ database_name = os.environ['ORACLE_DATABASE']
 
 class database():
     def __init__(self):
-        self.connection = self.connect()
-    def connect(self):
+        self.connection = self.connectToDB()
+    def connectToDB(self):
         with cx_Oracle.connect(username, password, database_name) as connection:
             return connection
 
     def loginSQLOnlyPass(self, sqlpass):
-        cursor = self.connection.cursor()
-        data = cursor.execute('select count(m.keyid) from man m WHERE m.password = %s' % sqlpass)
-        if data[0] > 0:
-            return True
-        else:
+        with cx_Oracle.connect(username, password, database_name) as connection:
+            cursor = connection.cursor()
+            data = cursor.execute("select count(m.keyid) from man m WHERE m.pass = '%s' " % sqlpass)
+            data = data.fetchone()
+            if data[0] > 0:
+                return True
+            else:
+                return False
             return False
-        return False
